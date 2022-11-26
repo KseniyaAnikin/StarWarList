3ca85e0fedd65cc7f982d73cf3c54df97d52eb71
KseniyaAnikin <100955276+KseniyaAnikin@users.noreply.github.com>
2022-11-26 13:14:36 +0700

    check

--- film-details.js
+++ film-details.js
@@ -1,9 +1,53 @@
+const container = document.createElement('div');
+
 export function render(data) {
   console.log(data)
-  const container = document.createElement('div');
-  container.classList.add('container');
-  container.style.width = "50%";
-  container.style.margin = "auto";
-  container.innerHTML = `<h1>${data.title} (Эпизод ${data.episode_id})</h1>`
+  container.classList.add('container' ,'max-w-4xl' , 'mx-auto');
+  container.innerHTML = `<h1 class="text-center text-purple-700">${data.title} (Эпизод ${data.episode_id})</h1>
+  <p class="italic text-center text-blue-800"> ${data.opening_crawl} </p>`
+  getData([data.planets, data.species, data.starships]);
+
+  // backBtn.classList.add('btn', 'btn-info');
+  // backBtn.textContent = "Back to episodes";
+  // container.append(backBtn);
   return container;
 }
+
+function getData(arrUrl){
+let i = 0
+  arrUrl.map(src => Promise.all(src.map(url => fetch(url).then(res =>res.json()))).then( arr => {
+
+    console.log(arr)
+    i++;
+    let title = document.createElement('h2');
+
+    if(i === 1) {
+      title.textContent = "Planets"
+    }
+    else if(i === 2){
+      title.textContent = "Species"
+    }
+    else if(i === 3){
+      title.textContent = "Star Ships"
+    }
+    title.classList.add('text-center', 'text-indigo-700');
+    container.append(title);
+    let list = document.createElement('ul');
+    container.append(list);
+    arr.map(el => {
+      let item = document.createElement('li');
+      item.classList.add('text-center');
+      item.textContent = `${el.name}`;
+      list.append(item)
+    })
+  }))
+}
+
+// function getBtn(){
+//   document.addEventListener('DOMContentLoaded', ()=>{
+//     let backBtn = document.createElement('button');
+//     backBtn.classList.add('btn', 'btn-info');
+//     backBtn.textContent = "Back to episodes";
+//     container.append(backBtn);
+//   })
+// }
--- film-list.js
+++ film-list.js
@@ -1,10 +1,7 @@
 export function render(data){
 
   const list = document.createElement('div');
-  list.classList.add('list-group');
-  list.style.width = "50%";
-  list.style.margin = "auto";
-
+  list.classList.add('list-group', 'max-w-4xl' , 'mx-auto')
   let arr = data.results;
 
   let i = 1;
@@ -35,6 +32,7 @@ export function render(data){
     filmName.textContent = element.title + ` (Эпизод ${n})`;
     filmName.classList.add('list-group-item', 'list-group-item-action');
     filmName.href = `?films=${i++}`;
+    filmName.classList.add('text-2xl' ,'font-bold', 'underline')
     list.append(filmName);
   });
 
